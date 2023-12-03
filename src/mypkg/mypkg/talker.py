5d42c972ac557f97566633835687e20eef50d4d6
Ryousuke Ochiai <s22C1028FL@s.chibakoudai.jp>
2023-12-04 00:21:01 +0900

    Add a commnd

--- src/mypkg/mypkg/talker.py
+++ src/mypkg/mypkg/talker.py
@@ -3,12 +3,12 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class Talker():          #ヘッダの下にTalkerというクラスを作成
-    def __init__(self, nh):  # オブジェクトを作ると呼ばれる関数
+    def __init_(self, nh):  # オブジェクトを作ると呼ばれる関数
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
         # ↑ selfはオブジェクトのこと
         # ↑ オブジェクトにひとつパブリッシャと変数をもたせる。
-        nh.create_timer(0.5, cb) #ここでタイマーをしかける。
+        nh.create_timer(0.5, self.b) #ここでタイマーをしかける。
 
     def cb(self):              #関数内のnやpubをtalkerのものに変更
         msg = Int16()
@@ -16,7 +16,11 @@ class Talker():          #ヘッダの下にTalkerというクラスを作成
         self.pub.publish(msg)
         self.n += 1
 
-rclpy.init()
-node = Node("talker")
-talker = Talker()      #オブジェクトを作成（__init__が実行される。）
-rclpy.spin(node)
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
